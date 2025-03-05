59c7d1f85ffc2d4096a59cbb89300ea061143d6f
Gaurav Pawse <gauravpawse664@gmail.com>
2025-03-05 11:32:46 +0530

    Regenerated package-lock.json

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,4 @@
 import AboutSectionOne from "@/components/About/AboutSectionOne";
-import AboutSectionTwo from "@/components/About/AboutSectionTwo";
 import Breadcrumb from "@/components/Common/Breadcrumb";
 import Projects from "@/components/Projects";
 
